7e92bbf35135e7f915478ab48b98c9e50d1cc82d
chopha <parcon1999@gmail.com>
2025-01-02 15:43:14 +0900

    fix SP_ACTOR_RANDOM_EXEC.sql

--- src/main/webapp/DB/SP_ACTOR_RANDOM_EXEC.sql
+++ src/main/webapp/DB/SP_ACTOR_RANDOM_EXEC.sql
@@ -1,20 +1,30 @@
 DECLARE
     iCur SYS_REFCURSOR; -- 결과를 받을 커서
-    v_actor_id             NUMBER;
+    v_actor_id             NUMBER(10);
     v_actor_name           NVARCHAR2(50);
-    v_face_code            NUMBER;
+    v_face_code            NUMBER(1);
+    v_face_name            NVARCHAR2(50);
+    v_actor_debut          NVARCHAR2(100);
+    v_actor_hit            NVARCHAR2(100);
+    v_actor_newest         NVARCHAR2(100);
+    v_actor_profile_photo  NVARCHAR2(255);
     v_actor_worldcup_photo NVARCHAR2(255);
 
 BEGIN
-    -- 총 16개의 데이터를 가져오는 경우
+    -- 총 32개의 데이터를 가져오는 경우
     SP_ACTOR_RANDOM(32, iCur);
     
     -- 커서 데이터 출력
     LOOP
-        FETCH iCur INTO v_actor_id, v_actor_name, v_face_code, v_actor_worldcup_photo;
+        FETCH iCur INTO v_actor_id, v_actor_name, v_face_code, v_face_name, 
+                        v_actor_debut, v_actor_hit, v_actor_newest, 
+                        v_actor_profile_photo, v_actor_worldcup_photo;
         EXIT WHEN iCur%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE('actor_id: ' || v_actor_id || ', actor_name: ' || v_actor_name || 
-                             ', face_code: ' || v_face_code || ', worldcup_photo: ' || v_actor_worldcup_photo);
+      DBMS_OUTPUT.PUT_LINE('actor_id: ' || v_actor_id || ', actor_name: ' || v_actor_name || 
+                             ', face_code: ' || v_face_code || ', face_name: ' || v_face_name || 
+                             ', actor_debut: ' || v_actor_debut || ', actor_hit: ' || v_actor_hit || 
+                             ', actor_newest: ' || v_actor_newest || ', profile_photo: ' || v_actor_profile_photo || 
+                             ', worldcup_photo: ' || v_actor_worldcup_photo);
     END LOOP;
     
     -- 커서 닫기
