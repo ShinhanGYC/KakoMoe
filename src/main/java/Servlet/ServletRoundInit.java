de8eda586b4111eb335d89969db3a8e1ba737fe7
chopha <parcon1999@gmail.com>
2025-01-07 16:26:13 +0900

    add Comment in Servlet file

--- src/main/java/Servlet/ServletRoundInit.java
+++ src/main/java/Servlet/ServletRoundInit.java
@@ -1,3 +1,9 @@
+// #################################################################################################
+// ServletRoundInit.java - 서블릿 이상형 월드컵 라운드 초기 설정 모듈 (DB에서 배우 정보 추출 + 리스트 생성 +  리스트 셔플 + Session 설정 + ServletRoundPlay 전송) 
+// #################################################################################################
+// ═════════════════════════════════════════════════════════════════════════════════════════
+// 외부모듈 영역
+// ═════════════════════════════════════════════════════════════════════════════════════════
 package Servlet;
 
 import java.io.IOException;
@@ -14,51 +20,140 @@ import javax.servlet.http.HttpSession;
 import BeansHome.Actor.ActorDAO;
 import BeansHome.Actor.ActorDTO;
 import java.util.Collections;
-/**
- * Servlet implementation class roundInitServlet
- */
-@WebServlet("/roundInitServlet")
-public class roundInitServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-   
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public roundInitServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+//═════════════════════════════════════════════════════════════════════════════════════════
+//사용자정의 클래스 영역
+//═════════════════════════════════════════════════════════════════════════════════════════
+/***********************************************************************
+* ServletRoundInit			: 서블릿 이상형 월드컵 라운드 초기 설정 클래스<br>
+* Inheritance				: HttpServlet
+***********************************************************************/
+@WebServlet("/ServletRoundInit")
+public class ServletRoundInit extends HttpServlet {
+	
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역상수 관리 - 필수영역
+	// —————————————————————————————————————————————————————————————————————————————————————
+	/** serialVersionUID : Bean 객체(메모리 객체 데이터) 직렬화 아이디(Servlet 구현시 필수 사항) */
+	private static final long serialVersionUID = 1L;
+	
+	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역변수 관리 - 필수영역(정적변수) : JSP 쪽에 노출시키려면 Static 등록 필요!
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// /** DBMgr : 오라클 데이터베이스 DAO 객체 선언 */
+ 	// public static DAO.DBOracleMgr DBMgr = null;
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역변수 관리 - 필수영역(인스턴스변수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+     // 생성자 관리 - 필수영역(인스턴스함수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	/***********************************************************************
+ 	 * ServletRoundInit()		: 생성자
+ 	 * @param void				: None
+ 	 ***********************************************************************/
+    public ServletRoundInit() {
+        super();
+        
+        try {
+        	// -----------------------------------------------------------------------------
+			// 기타 초기화 작업 관리
+			// -----------------------------------------------------------------------------
+			Common.ExceptionMgr.SetMode(Common.ExceptionMgr.RUN_MODE.DEBUG);
+			// -----------------------------------------------------------------------------
+        }
+        catch (Exception Ex)
+		{
+			Common.ExceptionMgr.DisplayException(Ex);	// 예외처리(콘솔)
+		}
+    }
+    
+    // —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역함수 관리 - 필수영역(정적함수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역함수 관리 - 필수영역(인스턴스함수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	/***********************************************************************
+ 	 * doGet()			: Servlet doGet 함수 선언(필수 함수)
+ 	 * 					: get 방식으로 파라미터를 전달하는 경우 호출됨
+ 	 * @param request	: 웹 서버 요청에 사용하는 파라미터
+ 	 * @param response	: 웹 서버 응답시 사용하는 파라미터
+ 	 * @return void		: None
+ 	 * @exception ServletException
+ 	 * @exception IOException
+ 	 ***********************************************************************/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		boolean bContinue = false;   
-		// TODO Auto-generated method stub
-        request.setCharacterEncoding("UTF-8");
-        String requestURI = request.getParameter("round");
-        String round	 = requestURI.replaceAll("[^0-9]","");
-		System.out.println("roundInitServlet round : " + round);
+		try{
+			doPost(request, response);
+		}
+		catch (Exception Ex){
+			Common.ExceptionMgr.DisplayException(Ex);	// 예외처리(콘솔)
+		}
+	}
+	
+	/***********************************************************************
+	 * doPost()			: Servlet doPost 함수 선언(필수 함수)
+	 * 					: post 방식으로 파라미터를 전달하는 경우 호출됨
+	 * @param request	: 웹 서버 요청에 사용하는 파라미터
+	 * @param response	: 웹 서버 응답시 사용하는 파라미터
+	 * @return void		: None
+	 * @exception ServletException
+	 * @exception IOException
+	 ***********************************************************************/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		ActorDAO ActorDAO = new ActorDAO();
+		boolean bContinue = false;									// DAO 실행 여부 ( true / false )
 		
+		// -----------------------------------------------------------------------------
+		// request parameter : round 파라미터 파싱 (String -> int) 
+		// -----------------------------------------------------------------------------
+        String requestURI = null;		// round 값 파싱 : (32강 / 16강) 변수
+        String roundStr	  = null;		// 숫자 값 파싱   : (32 / 16) 변수
+		int round 	      = 0;			// String -> Integer 형 변환 변수
 		
-		ArrayList<ActorDTO> ActorDTOList = new ArrayList<>();
+		ActorDAO ActorDAO 	= null;		// 배우 DAO Bean 객체(Database Access Object)
+		ArrayList<ActorDTO> ActorDTOList = null; // 배우 배열 리스트 DTO Bean 객체(Database Access Object)
 		
-		
-		
+		HttpSession session = null;		// 세션 객체
+		RequestDispatcher dispatcher = null; // forword 객체 
 		
 		try {
-			if (ActorDAO.takeRandomActorForWorldcup(Integer.parseInt(round)) == true)
+			// -----------------------------------------------------------------------------
+			// 한글 인코딩 지정(가장 첫번째로 지정!)
+			// -----------------------------------------------------------------------------
+			request.setCharacterEncoding("UTF-8");					// 입력 된 파라미터 한글 인코딩
+			
+			// -----------------------------------------------------------------------------
+			// round 변수 값 설정
+			// -----------------------------------------------------------------------------
+			requestURI = request.getParameter("round");	
+			roundStr	 = requestURI.replaceAll("[^0-9]","");
+			round = Integer.parseInt(roundStr);		
+			
+			// -----------------------------------------------------------------	
+			// Beans 객체 생성
+			// -----------------------------------------------------------------
+			ActorDAO = new ActorDAO();		
+			ActorDTOList = new ArrayList<>();
+			
+	    	// -----------------------------------------------------------------------------
+			// DB에서 배우 데이터 랜덤으로 가져오기 (16개 or 32개)
+			// -----------------------------------------------------------------------------
+			if (ActorDAO.takeRandomActorForWorldcup(round) == true)
 			{
 				if (ActorDAO.DBMgr != null && ActorDAO.DBMgr.Rs != null)
 				{
 					bContinue = true;
 				}
-
 				
 				if (bContinue == true) {
+			    	// -----------------------------------------------------------------------------
+					// 가져온 배우 정보 배우 리스트 DTO에 삽입하기
+					// -----------------------------------------------------------------------------
 					while(ActorDAO.DBMgr.Rs.next() == true) {
 						ActorDTOList.add(new ActorDTO());
 						ActorDTOList.get(ActorDTOList.size()-1).setActorID(ActorDAO.DBMgr.Rs.getInt("actor_id"));
@@ -73,48 +168,43 @@ public class roundInitServlet extends HttpServlet {
 						}
 					}
 			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			
+			// -----------------------------------------------------------------	
+			// 선택된 배우 정보 출력 (서버 콘솔 창)
+			// -----------------------------------------------------------------
+			//        for(int i = 0; i < ActorDTOList.size(); i++)
+			//        {
+			//        	System.out.println("ActorDTOList.get(i).getActorID() : " + ActorDTOList.get(i).getActorID());
+			//			System.out.println("ActorDTOList.get(i).getActorName() : " + ActorDTOList.get(i).getActorName());
+			//			System.out.println("ActorDTOList.get(i).getFaceCode() : " + ActorDTOList.get(i).getFaceCode());
+			//			System.out.println("ActorDTOList.get(i).getActorWorldcupPhoto() : " + ActorDTOList.get(i).getActorWorldcupPhoto());
+			//        }
+			
+			// -----------------------------------------------------------------	
+			// 배우 리스트 셔플하기
+			// -----------------------------------------------------------------
+			Collections.shuffle(ActorDTOList);
+			
+			// -----------------------------------------------------------------	
+			// 세션 설정
+			// -----------------------------------------------------------------
+			session = request.getSession();			   		 // 세션값 가져오기
+	        session.setAttribute("totalRound", round); 		 // totalRound 파라미터 설정   	 | (16 / 32)
+	        session.setAttribute("currentRound", 1);   		 // currentRound 파라미터 설정 	 | 1
+	        session.setAttribute("actorList", ActorDTOList); // actorList 파라미터 설정 	 | ActorDTOList 객체 
+	        
+	      
+			// -----------------------------------------------------------------	
+			// 다음 서블릿에 데이터 전달 및 이동하기 
+			// -----------------------------------------------------------------
+	        dispatcher = request.getRequestDispatcher("ServletRoundPlay");
+	        dispatcher.forward(request, response);
+	        
+		} 
+		catch (Exception e) {
+			
 			e.printStackTrace();
 		}
-		
-
-        //request.getRequestDispatcher("worldcup.jsp").forward(request, response);
-        
-  
-        
-        
-        
-        for(int i = 0; i < ActorDTOList.size(); i++)
-        {
-        	System.out.println("ActorDTOList.get(i).getActorID() : " + ActorDTOList.get(i).getActorID());
-			System.out.println("ActorDTOList.get(i).getActorName() : " + ActorDTOList.get(i).getActorName());
-			System.out.println("ActorDTOList.get(i).getFaceCode() : " + ActorDTOList.get(i).getFaceCode());
-			System.out.println("ActorDTOList.get(i).getActorWorldcupPhoto() : " + ActorDTOList.get(i).getActorWorldcupPhoto());
-        }
-		
-		Collections.shuffle(ActorDTOList);
-        HttpSession session = request.getSession();
-        
-        session.setAttribute("totalRound", Integer.parseInt(round)); // round 값 전달
-        session.setAttribute("currentRound", 1);
-        session.setAttribute("list", ActorDTOList); // ActorDTOList 전달
-        
-        
-        RequestDispatcher dispatcher = request.getRequestDispatcher("roundPlayServlet");
-        dispatcher.forward(request, response);
-		
-	
 	}
-	
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-		
-	}
-	
 }
 
--- src/main/java/Servlet/ServletRoundPlay.java
+++ src/main/java/Servlet/ServletRoundPlay.java
@@ -1,123 +1,270 @@
+// #################################################################################################
+// ServletRoundPlay.java - 서블릿 이상형 월드컵 플레이 모듈 
+// #################################################################################################
+// ═════════════════════════════════════════════════════════════════════════════════════════
+// 외부모듈 영역
+// ═════════════════════════════════════════════════════════════════════════════════════════
 package Servlet;
 
 import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import BeansHome.Actor.ActorDAO;
+
 import BeansHome.Actor.ActorDTO;
-import java.util.ArrayList;
-import java.util.List;
-/**
- * Servlet implementation class roundPlayServlet
- */
-@WebServlet("/roundPlayServlet")
-public class roundPlayServlet extends HttpServlet {
+
+//═════════════════════════════════════════════════════════════════════════════════════════
+//사용자정의 클래스 영역
+//═════════════════════════════════════════════════════════════════════════════════════════
+/***********************************************************************
+* ServletRoundPlay			: 서블릿 이상형 월드컵 플레이 클래스<br>
+* Inheritance				: HttpServlet
+***********************************************************************/
+@WebServlet("/ServletRoundPlay")
+public class ServletRoundPlay extends HttpServlet {
+	
+	// —————————————————————————————————————————————————————————————————————————————————————
+	// 전역상수 관리 - 필수영역
+	// —————————————————————————————————————————————————————————————————————————————————————
+	/** serialVersionUID : Bean 객체(메모리 객체 데이터) 직렬화 아이디(Servlet 구현시 필수 사항) */
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public roundPlayServlet() {
+	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역변수 관리 - 필수영역(정적변수) : JSP 쪽에 노출시키려면 Static 등록 필요!
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// /** DBMgr : 오라클 데이터베이스 DAO 객체 선언 */
+ 	// public static DAO.DBOracleMgr DBMgr = null;
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역변수 관리 - 필수영역(인스턴스변수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+    // 생성자 관리 - 필수영역(인스턴스함수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	/***********************************************************************
+ 	 * ServletRoundPlay		()	: 생성자
+ 	 * @param void				: None
+ 	 ***********************************************************************/
+    public ServletRoundPlay() {
         super();
-        // TODO Auto-generated constructor stub
+        try {
+        	// -----------------------------------------------------------------------------
+			// 기타 초기화 작업 관리
+			// -----------------------------------------------------------------------------
+			Common.ExceptionMgr.SetMode(Common.ExceptionMgr.RUN_MODE.DEBUG);
+			// -----------------------------------------------------------------------------
+        }
+        catch (Exception Ex)
+		{
+			Common.ExceptionMgr.DisplayException(Ex);	// 예외처리(콘솔)
+		}
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 HttpSession session = request.getSession();
-		
-		 ArrayList<ActorDTO> currentActorList =  (ArrayList) session.getAttribute("list");
-		 for (ActorDTO actorDTO : currentActorList) {
-			 System.out.println(actorDTO.getActorName());
-		 }
-		 int currentRound = 0;
-		 int totalRound = 0;
-		 if (session.getAttribute("currentRound") == null) {
-			 currentRound = 1;
-			 session.setAttribute("currentRound", currentRound);
-		 }
-		 else {
-			 currentRound= (Integer) session.getAttribute("currentRound");
-		 }
-		 
-		 if (session.getAttribute("totalRound") == null) {
-			 totalRound = 1;
-		 }
-		 else {
+    // —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역함수 관리 - 필수영역(정적함수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
 
-			totalRound = (Integer) session.getAttribute("totalRound");
-		 }
-		 
-		 if (currentActorList == null || currentActorList.isEmpty()) {
-			 response.sendRedirect("error.jsp");
-		 }
-		 if (currentActorList.size() == 1) {
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	// 전역함수 관리 - 필수영역(인스턴스함수)
+ 	// —————————————————————————————————————————————————————————————————————————————————————
+ 	/***********************************************************************
+ 	 * doGet()			: Servlet doGet 함수 선언(필수 함수)
+ 	 * 					: get 방식으로 파라미터를 전달하는 경우 호출됨
+ 	 * @param request	: 웹 서버 요청에 사용하는 파라미터
+ 	 * @param response	: 웹 서버 응답시 사용하는 파라미터
+ 	 * @return void		: None
+ 	 * @exception ServletException
+ 	 * @exception IOException
+ 	 ***********************************************************************/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = null;					 // 세션 객체
+		
+		ArrayList<ActorDTO> currentActorList = null; // 배우 배열 리스트 DTO Bean 객체
+		List<ActorDTO> currentPair = null;			 // 서로 대결 할 배우 리스트 DTO Bean 객체
+		
+		@SuppressWarnings("unused")
+		int currentRound = 0;						 // 현재 라운드 변수
+		@SuppressWarnings("unused")
+		int totalRound   = 0;						 // 전체 라운드 변수
+		
+		
+		
+		try {		
+			
+			session = request.getSession();
+				
+			// -------------------------------------------------------------------------------------------------------
+			// session 에서 설정되어 있는 currentRound 파라미터 읽기 
+			// 성공 : currentRound 변수 값 설정 
+			// 실패 : error.jsp 로 이동 |
+			// -------------------------------------------------------------------------------------------------------
+			if (session.getAttribute("currentRound") == null) {
+				response.sendRedirect("error.jsp");
+			}
+			else {
+				currentRound = (Integer) session.getAttribute("currentRound");
+			}
+ 
+			// ---------------------------------------------------------------------------------------------------
+			// session 에서 설정되어 있는 totalRoynd 파라미터 읽기 
+			// 성공 : totalRound 변수 값 설정 
+			// 실패 : error.jsp 로 이동 |
+			// ---------------------------------------------------------------------------------------------------
+			if (session.getAttribute("totalRound") == null) {
+				response.sendRedirect("error.jsp");
+			}
+			else {
+				totalRound = (Integer) session.getAttribute("totalRound");
+			}
+ 
+			// -------------------------------------------------------------------------------------------------------------
+			// session 에서 설정되어 있는 actorList 파라미터 읽기 
+			// 성공 : currentActorList 배열 리스트 값 설정 
+			// 실패 : error.jsp 로 이동 |
+			// -------------------------------------------------------------------------------------------------------------
+			if ((ArrayList<ActorDTO>) session.getAttribute("actorList") == null) {
+				response.sendRedirect("error.jsp");
+			}
+			else {
+				currentActorList =  (ArrayList<ActorDTO>) session.getAttribute("actorList");
+			}
+ 
+ 
+			// --------------------------------------------------------------------------------------------------------------------------------------------------------
+			// currentActorList 사이즈 읽기 
+			// 1 일 경우 : 우승자를 winnerActor 파라미터 설정 후 winner.jsp로 이동 
+			// 그 외 :두명의 배우를 currentPair에 설정 후 play.jsp로 이동하여 게임 진행 |
+			// --------------------------------------------------------------------------------------------------------------------------------------------------------
+			if (currentActorList.size() == 1) {
 			 session.setAttribute("winnerActor", currentActorList.get(0));
 			 response.sendRedirect("winner.jsp");			
-		 }
-		 else {
-			 System.out.println(totalRound);
-			 System.out.println(currentRound);
-			 List<ActorDTO> pairList = currentActorList.subList(0, 2);
-			 session.setAttribute("currentPair", pairList);
+			}
+			else {
+			 currentPair = currentActorList.subList(0, 2);
+			 session.setAttribute("currentPair", currentPair);
 			 response.sendRedirect("play.jsp");
-		 }
+			}
+	
+		}
+		catch(Exception Ex){
+			Common.ExceptionMgr.DisplayException(Ex);	// 예외처리(콘솔)
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	/***********************************************************************
+	 * doPost()			: Servlet doPost 함수 선언(필수 함수)
+	 * 					: post 방식으로 파라미터를 전달하는 경우 호출됨
+	 * @param request	: 웹 서버 요청에 사용하는 파라미터
+	 * @param response	: 웹 서버 응답시 사용하는 파라미터
+	 * @return void		: None
+	 * @exception ServletException
+	 * @exception IOException
+	 ***********************************************************************/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession session = request.getSession();
-		ArrayList<ActorDTO> currentActorList =  (ArrayList) session.getAttribute("list");
-		int currentRound = (Integer) session.getAttribute("currentRound");
-		int totalRound = (Integer) session.getAttribute("totalRound");
-		if(currentActorList == null || currentActorList.size() <= 1) {
-			response.sendRedirect("roundPlayServlet");
+		
+		HttpSession session = null; 						// 세션 객체
+		
+		ArrayList<ActorDTO> currentActorList = null;		// 배우 배열 리스트 DTO Bean 객체
+		ArrayList<ActorDTO> nextRoundActorList  = null;	 	// 서로 대결 할 배우 리스트 DTO Bean 객체
+		
+		int currentRound = 0;								// 현재 라운드 변수
+		int totalRound   = 0;								// 전체 라운드 변수
+		
+		String selectedActorId = null;						// 선택된 배우 ID
+		try {
 			
-		}
-		String selectedActorId = request.getParameter("selectedActor");
-		ArrayList<ActorDTO> nextRoundActorList = (ArrayList) session.getAttribute("nextRoundActorList");
-	
-		if(selectedActorId != null) {
-			if(nextRoundActorList == null) {
-				nextRoundActorList = new ArrayList<>();
+			session = request.getSession();
+			
+			// -----------------------------------------------------------------------------
+			// crrentActorList 의 값 설정 : actorList 파라미터 에서 읽기 
+			// -----------------------------------------------------------------------------
+			currentActorList =  (ArrayList<ActorDTO>) session.getAttribute("actorList");
+			
+			// -----------------------------------------------------------------------------
+			// currentRound 의 값 설정 : currentRound 파라미터 에서 읽기 
+			// -----------------------------------------------------------------------------
+			currentRound = (Integer) session.getAttribute("currentRound");
+			
+			// -----------------------------------------------------------------------------
+			// totalRound 의 값 설정 : totalRound 파라미터 에서 읽기 
+			// -----------------------------------------------------------------------------
+			totalRound = (Integer) session.getAttribute("totalRound");
+			
+			// -----------------------------------------------------------------------------
+			// selectedActorId 의 값 설정 : selectedActor 파라미터 에서 읽기 
+			// -----------------------------------------------------------------------------
+			selectedActorId = request.getParameter("selectedActor");
+			
+			// -----------------------------------------------------------------------------
+			// nextRoundActorList 의 값 설정 : nextRoundActorList 파라미터 에서 읽기 
+			// -----------------------------------------------------------------------------
+			nextRoundActorList = (ArrayList<ActorDTO>)session.getAttribute("nextRoundActorList");
+			
+			
+			
+			// -----------------------------------------------------------------------------
+			// (32강,16강,8강,4강 라운드가 끝날때 마다 servletRoundPlay 호출
+			// -----------------------------------------------------------------------------
+			if(currentActorList == null || currentActorList.size() <= 1) {
+				response.sendRedirect("ServletRoundPlay");
+				
 			}
-			for (ActorDTO actorDTO : currentActorList) {
-				if(actorDTO.getActorID() == (Integer.parseInt(selectedActorId))) {
-					nextRoundActorList.add(actorDTO);
-					break;
+			
+			// -----------------------------------------------------------------------------
+			// 선택된 배우를 nextRoundActorList에 추가하기
+			// -----------------------------------------------------------------------------
+			if(selectedActorId != null) {
+				if(nextRoundActorList == null) {
+					nextRoundActorList = new ArrayList<>();
 				}
+				for (ActorDTO actorDTO : currentActorList) {
+					if(actorDTO.getActorID() == (Integer.parseInt(selectedActorId))) {
+						nextRoundActorList.add(actorDTO);
+						break;
+					}
+				}
+				session.setAttribute("nextRoundActorList", nextRoundActorList);
 			}
-			for (ActorDTO actorDTO : nextRoundActorList) {
-				System.out.println("C:" + actorDTO.getActorName());
+		
+			// -----------------------------------------------------------------------------
+			// 남아있는 배우가 2명일 경우 결승전으로 우승자 뽑기
+			// -----------------------------------------------------------------------------
+			if(currentActorList.size() == 2) {
+				session.setAttribute("actorList", nextRoundActorList);
+				session.setAttribute("nextRoundActorList", null);
+		
+				session.setAttribute("currentRound", 1);
+				session.setAttribute("totalRound", totalRound / 2);
 			}
-			session.setAttribute("nextRoundActorList", nextRoundActorList);
+			// -----------------------------------------------------------------------------
+			// 그 외에는 계속 게임 진행
+			// -----------------------------------------------------------------------------
+			else {
+				currentActorList.remove(0);
+				currentActorList.remove(0);
+				session.setAttribute("currentRound", currentRound + 1);
+				session.setAttribute("actorList", currentActorList);
+	
+			}
+			// -----------------------------------------------------------------------------
+			// 설정 완료 후 ServletRoundPlay(get) 요청 
+			// -----------------------------------------------------------------------------
+			response.sendRedirect("ServletRoundPlay");
 		}
-	
-		if(currentActorList.size() == 2) {
-			session.setAttribute("list", nextRoundActorList);
-			session.setAttribute("nextRoundActorList", null);
-	
-			session.setAttribute("currentRound", 1);
-			session.setAttribute("totalRound", totalRound / 2);
-		}else {
-			currentActorList.remove(0);
-			currentActorList.remove(0);
-			session.setAttribute("currentRound", currentRound + 1);
-			session.setAttribute("list", currentActorList);
+		catch (Exception e) {
+			e.printStackTrace();
 			
-		
 		}
 		
-		response.sendRedirect("roundPlayServlet");
+		
 		
 	}
 
